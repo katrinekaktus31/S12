5b8f907e008aabe8a66678cf5dd0030d916c87d2
Kateryna Brudko <katyabru31@gmail.com>
2020-06-16 14:23:06 +0300

    create a final version 2.0 s12

--- s_12.py
+++ s_12.py
@@ -3,26 +3,26 @@ import arcpy
 arcpy.env.workspace = arcpy.GetParameterAsText(0)
 arcpy.env.overwriteOutput = True
 amenities = ['school', 'hospital', 'place_of_worship']
-country = "El Salvador"
-try:
-    arcpy.CreateFileGDB_management(arcpy.env.workspace, 'S_12.gdb')
-    arcpy.AddMessage("Create new FileGDB " + 'S_12.gdb')
-    # Make a feature layer
-    arcpy.MakeFeatureLayer_management('CentralAmerica.shp', 'SelectCountryLayer', '"NAME" ' + "'" + country + "'")
-    arcpy.MakeFeatureLayer_management('OSMpoints.shp', 'SelectOSMpointsLayer', )
-    arcpy.AddMessage("Create feature layer SelectCountryLayer, SelectOSMpointsLayer" )
+CentralAmerica = arcpy.GetParameterAsText(1)
+OSMpoints = arcpy.GetParameterAsText(2)
 
-    # Apply a selection
-    arcpy.SelectLayerByLocation_management("SelectCountryLayer", "CONTAINS", "SelectOSMpointsLayer")
-except:
-    print arcpy.GetMessages()
+country = arcpy.GetParameterAsText(3)
+
+arcpy.CreateFileGDB_management(arcpy.env.workspace, 'S_12_1.gdb')
+arcpy.AddMessage("Create new FileGDB " + 'S_12.gdb')
+# Make a feature layer
+arcpy.MakeFeatureLayer_management(CentralAmerica, 'SelectCountryLayer', '"NAME" = ' + "'"+country +"'")
+arcpy.MakeFeatureLayer_management(OSMpoints, 'SelectpointsLayer', )
+arcpy.AddMessage("Create feature layer SelectCountryLayer, SelectOSMpointsLayer")
+# Apply a selection
+arcpy.SelectLayerByLocation_management("SelectCountryLayer", "CONTAINS", "SelectpointsLayer")
 
 for i in amenities:
-    arcpy.SelectLayerByAttribute_management("SelectOSMpointsLayer", "ADD_TO_SELECTION", '"amenity" = ' + "'" + i + "'")
+    arcpy.SelectLayerByAttribute_management("SelectpointsLayer", "ADD_TO_SELECTION", '"amenity" = ' + "'" + i + "'")
     # Copy the features to a new feature class and clean up
     arcpy.AddMessage("Create selection")
     arcpy.AddMessage("Create new feature layer FC" + i)
-    arcpy.CopyFeatures_management("SelectOSMpointsLayer", arcpy.env.workspace + r'\S_12.gdb.' + '\\FC' + i)
+    arcpy.CopyFeatures_management("SelectpointsLayer", arcpy.env.workspace + r'\S_12.gdb.' + '\\FC' + i)
     arcpy.AddField_management('S_12.gdb.' + '\\FC' + i, 'source', "TEXT")
     arcpy.AddMessage("Create new field 'source' in FC" + i)
     arcpy.AddField_management('S_12.gdb.' + '\\FC' + i, 'GID', "DOUBLE")
